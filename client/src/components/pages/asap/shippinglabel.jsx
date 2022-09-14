b2dfee6eb2ed44653f89279c64f965969f8e0a67
nyteghost <nytemmo@gmail.com>
2022-09-14 16:57:54 -0400

    Added CSS for

--- client/src/components/pages/asap/shippinglabel.jsx
+++ client/src/components/pages/asap/shippinglabel.jsx
@@ -5,79 +5,76 @@ import ReactToPrint from 'react-to-print';
 import {Box} from "@mui/material"
 
 
-const pageStyle = ``;
+const pageStyle = `@page {
+    size: 4in 6in;
+}`
 
 const ComponentToPrint = React.forwardRef((props, ref) => {
     props = props.props
     
     return(
-        <Box 
-            sx={{
-           bg: 'white'
-           }}
-        >
-            <div ref={ref}>
-            <section class="flexed">
-                    <div>
-                        <p>Southeastern Computer Associates</p>
-                        <p>111 Sparks Drive</p>
-                        <p>Suite 6008</p>
-                        <p>Hiram GA 30141</p>
-                    </div>
-                    <div>
-                        <p>ASAP Shipping Label</p>
-                    </div>
-                    <div>
-                        <p>Date Printed:{props.date}</p>
-                    </div>
-            </section>
-                <br />
-                <section style={{align: 'left'}}>
-                <p style={{fontWeight: 'bold'}}>SHIP TO:</p>
-                    <div style={{marginLeft: 10}}>
-                        <p>{String(props.lg_name)}</p>
-                        <p>{String(props.phone)}</p>
-                        <p>{String(props.address)}</p>
-                        <p>{String(props.address_2)}</p>
-                        <p>{String(props.city)} {String(props.state)} {String(props.postal_code)}</p>
-                    
-                    </div>
+
+            <div ref={ref} className="PrintSection">
+                <section className="flexed">
+                        <div>
+                            <p className = 'p' >Southeastern Computer Associates</p>
+                            <p className = 'p' >111 Sparks Drive</p>
+                            <p className = 'p' >Suite 6008</p>
+                            <p className = 'p' >Hiram GA 30141</p>
+                        </div>
+                        <div>
+                            <p className = 'p' >ASAP Shipping Label</p>
+                        </div>
+                        <div>
+                            <p className = 'p' >Date Printed:{props.date}</p>
+                        </div>
                 </section>
-                <hr className='hr' />
-                <section class="flexed">
-                    <div>
-                    <img src={`https://api.qrserver.com/v1/create-qr-code/?data=${props.contact}&size=60x60&margin=0`}/>
-                    </div>
-                    <div style={{width: 200}}>
-                    </div>
-                    <div>
-                        <p style={{fontSize: 30}}>{props.contact}</p>
-                    </div>
-                </section>
-                <hr className='hr'/>
-                <section class="flexed">    
-                    <div>
-                        <p style={{fontSize: 30}}>GCA-{props.assetid}</p>
-                    </div>
-                    <div style={{width: 200}}>
-                    </div>
-                    <div>
-                        <img src={ `https://api.qrserver.com/v1/create-qr-code/?data=${props.assetid}&size=60x60&margin=0`}/>                
-                    </div>
-                </section>
-                <hr className='hr'/>
-                <section class="flexed">
-                    <div>
-                        <img src={`https://api.qrserver.com/v1/create-qr-code/?data=${props.trackingid}&size=60x60&margin=0`}/>                
-                    </div>
-                    <div style={{width: 200}}>
-                    </div>
-                    <div>
-                        <p style={{fontSize: 30}}>{props.trackingid}</p>
-                    </div>
-                </section>
+                    <br />
+                    <section>
+                    <p className = 'p'  style={{fontWeight: 'bold'}}>SHIP TO:</p>
+                        <div style={{marginLeft: 10}}>
+                            <p className = 'p' >{String(props.lg_name)}</p>
+                            <p className = 'p' >{String(props.phone)}</p>
+                            <p className = 'p' >{String(props.address)}</p>
+                            <p className = 'p' >{String(props.address_2)}</p>
+                            <p className = 'p' >{String(props.city)} {String(props.state)} {String(props.postal_code)}</p>
+                        
+                        </div>
+                    </section>
+                    <hr className='hr' />
+                    <section class="flexed">
+                        <div>
+                        <img src={`https://api.qrserver.com/v1/create-qr-code/?data=${props.contact}&size=100x100&margin=0`}/>
+                        </div>
+                        <div style={{width: 200}}>
+                        </div>
+                        <div>
+                            <p className = 'p'  style={{fontSize: 30}}>{props.contact}</p>
+                        </div>
+                    </section>
+                    <hr className='hr'/>
+                    <section class="flexed">    
+                        <div>
+                            <p className = 'p'  style={{fontSize: 30}}>GCA-{props.assetid}</p>
+                        </div>
+                        <div style={{width: 200}}>
+                        </div>
+                        <div>
+                            <img src={ `https://api.qrserver.com/v1/create-qr-code/?data=${props.assetid}&size=100x100&margin=0`}/>                
+                        </div>
+                    </section>
+                    <hr className='hr'/>
+                    <section class="flexed">
+                        <div>
+                            <img src={`https://api.qrserver.com/v1/create-qr-code/?data=${props.trackingid}&size=100x100&margin=0`}/>                
+                        </div>
+                        <div style={{width: 200}}>
+                        </div>
+                        <div>
+                            <p className = 'p'  style={{fontSize: 30}}>{props.trackingid}</p>
+                        </div>
+                    </section>
             </div>
-        </Box>
     )
 });
 
@@ -89,7 +86,7 @@ export const ShippingLabel = (props) => {
     return (
         <div>
           <ReactToPrint
-            pageStyle={pageStyle}
+           
             trigger={() => <button id="printButton">Print this out!</button>}
             content={() => componentRef.current}
           />
